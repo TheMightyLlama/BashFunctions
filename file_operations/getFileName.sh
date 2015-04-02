1b2fc5da794f1c41c3285296f07e774f7995c913
Christian Macedo <cmacedo@orange-vallee.net>
2015-04-02 15:48:35 +0100

    extra annotation

--- file_operations/getFileName.sh
+++ file_operations/getFileName.sh
@@ -2,12 +2,16 @@
 
 function getFileName () {
     
+    #Passed value
+    filename=$1;
+
     #Remove directories
     filename=${1##*/}
 
     #Remove extension
     filename=${filename%.*}
 
+    #Return name of file
     echo $filename;
 
 }
